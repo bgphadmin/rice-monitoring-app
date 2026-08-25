4d1d439c32174058a0618adecca69b828fb6bedf
20220255 <118017744+20220255@users.noreply.github.com>
2026-08-25 20:00:21 +0800

    removed comment on employee grid

--- components/employee/EmployeeGrid.tsx
+++ components/employee/EmployeeGrid.tsx
@@ -97,7 +97,6 @@ export default function EmployeeGrid({
         }, 2000)
 
         return () => clearTimeout(handler)
-    // }, [localFilter, onFilterChange, onPaginationChange, pagination.pageSize, increment])
     }, [localFilter, onFilterChange, onPaginationChange, pagination.pageSize])
 
     const table = useReactTable({
